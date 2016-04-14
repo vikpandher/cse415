924da1ff63d44b454ac607a143b3f404ed6858b3
CHLOE JEAN NASH <chloen2@uw.edu>
2016-04-13 17:27:02 -0700

    Progress, tracks relationships, working on questions

--- PlausibleArguments.py
+++ PlausibleArguments.py
@@ -13,27 +13,24 @@
 #  ('turtle' : ['reptile', 'shelled-creature'])
 
 from re import *   # Loads the regular expression module.
-# make an all powerful being like GOD for non qualified modes
-# person -> reliable, [cat, animal], 
-ISA = {}
-INCLUDES = {}
-ARTICLES = {}
-QUALIFIED = {}
+ISA = {} # [A is a [B, God], [c, joe]], [Jones is a reliable, God], [Stef is an unreliable, Jones]
+INCLUDES = {} # B includes A, etc.
+ARTICLES = {} # (a) C
 
-
-def store_isa_fact(category1, category2):
+# Possibly add a parameter for 'speaker' make tuples.........
+def store_isa_fact(category1, category2, speaker):
     'Stores one fact of the form A BIRD IS AN ANIMAL'
     # That is, a member of CATEGORY1 is a member of CATEGORY2
     try :
         c1list = ISA[category1]
-        c1list.append(category2)
+        c1list.append([category2, speaker])
     except KeyError :
-        ISA[category1] = [category2]
+        ISA[category1] = [[category2, speaker]]
     try :
         c2list = INCLUDES[category2]
-        c2list.append(category1)
+        c2list.append([category1, speaker])
     except KeyError :
-        INCLUDES[category2] = [category1]
+        INCLUDES[category2] = [[category1, speaker]]
         
 def get_isa_list(category1):
     'Retrieves any existing list of things that CATEGORY1 is a'
@@ -54,7 +51,11 @@ def get_includes_list(category1):
 def isa_test1(category1, category2):
     'Returns True if category 2 is (directly) on the list for category 1.'
     c1list = get_isa_list(category1)
-    return c1list.__contains__(category2)
+    for isa in c1list :
+        if isa[0] == category2 :
+            return True
+    return False
+    # return c1list.__contains__(category2)
     
 def isa_test(category1, category2, depth_limit = 10):
     'Returns True if category 1 is a subset of category 2 within depth_limit levels'
@@ -62,10 +63,27 @@ def isa_test(category1, category2, depth_limit = 10):
     if isa_test1(category1, category2) : return True
     if depth_limit < 2 : return False
     for intermediate_category in get_isa_list(category1):
-        if isa_test(intermediate_category, category2, depth_limit - 1):
+        if isa_test(intermediate_category[0], category2, depth_limit - 1):
             return True
     return False
 
+# Checks if any speakers other than God
+def check_speakers(x, y) :
+    c1list = get_isa_list(x)
+    for isa in c1list :
+        if isa[0] == y and isa[1] == "God" :
+            return True
+    return False
+
+def check_speakers2(x, y, depth_limit = 10) :
+    if x == y : return True
+    if check_speakers(x, y) : return True
+    if depth_limit < 2 : return False
+    for intermediate_category in get_isa_list(x):
+        if check_speakers2(intermediate_category[0], y, depth_limit - 1):
+            return True
+    return False
+    
 def store_article(noun, article):
     'Saves the article (in lower-case) associated with a noun.'
     ARTICLES[noun] = article.lower()
@@ -86,44 +104,67 @@ def linneus():
         info = input('Enter an ISA fact, or "bye" here: ')
         if info == 'bye': return 'Goodbye now!'
         process(info)
+        print_dict() ###
+        print() ###
 
 # Some regular expressions used to parse the user sentences:    
-assertion_pattern = compile(r"^(a|an|A|An)\s+([-\w]+)\s+is\s+(a|an)\s+([-\w]+)(\.|\!)*$", IGNORECASE)    
+assertion_pattern = compile(r".*(a|an|A|An)\s+([-\w]+)\s+is\s+(a|an)\s+([-\w]+)(\.|\!)*$", IGNORECASE)    
 query_pattern = compile(r"^is\s+(a|an)\s+([-\w]+)\s+(a|an)\s+([-\w]+)(\?\.)*", IGNORECASE)    
 what_pattern = compile(r"^What\s+is\s+(a|an)\s+([-\w]+)(\?\.)*", IGNORECASE)    
 why_pattern = compile(r"^Why\s+is\s+(a|an)\s+([-\w]+)\s+(a|an)\s+([-\w]+)(\?\.)*", IGNORECASE)    
-qualified_pattern = compile(r"^([-\w]+)\s+says\s+that(.*)", IGNORECASE)
-reliablity_pattern = compile(r"^([-\w]+)\s+is\s+a\s+(reliable|unreliable)\s+source$", IGNORECASE)
+# see if qualified
+qualified_pattern = compile(r"^([-\w]+)\s+says\s+that.*", IGNORECASE)
+reliability_pattern = compile(r"^([-\w]+\s+says\s+that\s+)?([-\w]+)\s+is\s+(a|an)\s+(reliable|unreliable)\s+source$", IGNORECASE)
 
 def process(info) :
+    # in case not qualified
+    speaker = "God"
     result_match_object = qualified_pattern.match(info)
     if result_match_object != None :
-        #then mark some flag
-        
+        # speaker is either the specified or default God
+        items = result_match_object.groups()
+        speaker = items[0]
+    result_match_object = reliability_pattern.match(info)
+    if result_match_object != None :
+        # speaker says items[0] is a items[2] source
+        items = result_match_object.groups()
+        store_article(items[3], items[2])
+        store_isa_fact(items[1], items[3], speaker)
+        print("I understand.")
+        return
     'Handles the user sentence, matching and responding.'
     result_match_object = assertion_pattern.match(info)
     if result_match_object != None :
+        # speaker says isa relationship
         items = result_match_object.groups()
         store_article(items[1], items[0])
         store_article(items[3], items[2])
-        store_isa_fact(items[1], items[3])
+        store_isa_fact(items[1], items[3], speaker)
         print("I understand.")
         return
     result_match_object = query_pattern.match(info)
     if result_match_object != None :
+        # have to check reliability of a speaker
         items = result_match_object.groups()
         answer = isa_test(items[1], items[3])
-        if answer :
-            print("Yes, it is.")
+        god = check_speakers2(items[1], items[3])
+        print(god)
+        print(answer)
+        if answer and god :
+            print("Yes.")
+        elif answer and not god :
+            print("It's possible")
+            #print("It's quite possible that " + get_article(items[1] +" " + str(items[1]) + " is " + get_article(items[3]) + " " + str(items[3]) + ".")
         else :
-            print("No, as far as I have been informed, it is not.")
+            print("I have no reason to believe so.")
         return
     result_match_object = what_pattern.match(info)
     if result_match_object != None :
+        # have to check statements, who said them, and their reliability
         items = result_match_object.groups()
         supersets = get_isa_list(items[1])
         if supersets != [] :
-            first = supersets[0]
+            first = supersets[0][0]
             a1 = get_article(items[1]).capitalize()
             a2 = get_article(first)
             print(a1 + " " + items[1] + " is " + a2 + " " + first + ".")
@@ -131,7 +172,7 @@ def process(info) :
         else :
             subsets = get_includes_list(items[1])
             if subsets != [] :
-                first = subsets[0]
+                first = subsets[0][0]
                 a1 = get_article(items[1]).capitalize()
                 a2 = get_article(first)
                 print(a1 + " " + items[1] + " is something more general than " + a2 + " " + first + ".")
@@ -141,6 +182,7 @@ def process(info) :
         return
     result_match_object = why_pattern.match(info)
     if result_match_object != None :
+        # link reliabliity with who said what
         items = result_match_object.groups()
         if not isa_test(items[1], items[3]) :
             print("But that's not true, as far as I know!")
@@ -149,6 +191,7 @@ def process(info) :
         return
     print("I do not understand.  You entered: ")
     print(info)
+    
 
 def answer_why(x, y):
     'Handles the answering of a Why question.'
@@ -186,10 +229,16 @@ def find_chain(x, z):
         return [[x, z]]
     else:
         for y in get_isa_list(x):
-            if isa_test(y, z):
-                temp = find_chain(y, z)
-                temp.insert(0, [x,y])
+            if isa_test(y[0], z):
+                temp = find_chain(y[0], z)
+                temp.insert(0, [x,y[0]])
                 return temp
+
+### For Testing
+def print_dict() :
+    print("ISA      = " + str(ISA))
+    print("INCLUDES = " + str(INCLUDES))
+    print("ARTICLES = " + str(ARTICLES))
 
 def test() :
     process("A turtle is a reptile.")
