f0dbed9d881c3efddb0fbae5a6feaef83839373a
Vik Pandher <vikpandher@gmail.com>
2016-05-04 15:43:19 -0700

    BC player

---
+++ Assignment5/alpha_BC_Player.py
@@ -0,0 +1,26 @@
+'''TestAgent1.py
+Nobachess, implementation of an agent that can't play
+Baroque Chess.
+
+'''
+
+import baroque_succ as bcs
+import random
+
+def makeMove(currentState, currentRemark, timelimit):
+    newMoveDesc = 'No move'
+    newRemark = "I don't even know how to move!"
+    return [[newMoveDesc, currentState], newRemark]
+
+def nickname():
+    return "Nobachess"
+
+def introduce():
+    return "I'm Nobachess, I don't play baroque chess at this time."
+
+def prepare(player2Nickname):
+    pass
+
+
+
+
