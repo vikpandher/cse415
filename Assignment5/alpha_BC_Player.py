d7e0c2a63cf8bb7475b5dd2670f14ed34b0a72ca
CHLOE JEAN NASH <chloen2@uw.edu>
2016-05-04 16:31:15 -0700

    initial stuff

--- Assignment5/alpha_BC_Player.py
+++ Assignment5/alpha_BC_Player.py
@@ -1,11 +1,39 @@
-'''TestAgent1.py
-Nobachess, implementation of an agent that can't play
-Baroque Chess.
+'''
+Vikramjit Pandher, Chloe Nash, CSE 415, Spring 2016, University of Washington
+Instructor:  S. Tanimoto.
+Assignment 5 Baroque Chess
+
+Status of the implementation:
+All required features working. Works with sample puzzles.
+
+Implementation of an Agent ___ that plays Baroque Chess.
+
+The initial board state is represented as:
+c l i w k i l f
+p p p p p p p p
+- - - - - - - -
+- - - - - - - -
+- - - - - - - -
+- - - - - - - -
+P P P P P P P P
+F L I W K I L C
+
+Where the starting board is shown using ASCII text, and the
+encoding is as follows: (lower case for black, upper case for WHITE):
+
+    p: pincer
+    l: leaper
+    i: imitator
+    w: withdrawer
+    k: king
+    c: coordinator
+    f: freezer
+    -: empty square on the board
 
 '''
 
-import baroque_succ as bcs
-import random
+#import baroque_succ as bcs
+#import random
 
 BLACK = 0
 WHITE = 1
@@ -62,6 +90,10 @@ def test_starting_board():
   init_state = BC_state(INITIAL, WHITE)
   print(init_state)
 
+# It should return a list of the form [[move, newState], newRemark].
+# The move is a data item describing the chosen move.
+# The newState is the result of making the move from the given currentState.
+# The newRemark to be returned must be a string.
 def makeMove(currentState, currentRemark, timelimit):
     newMoveDesc = 'No move'
     newRemark = "I don't even know how to move!"
@@ -71,15 +103,23 @@ def identifyPieces(currentState):
     our_pieces = []
     return
 
+# This function should return a short version of the playing agent's name.
 def nickname():
     return "Nobachess"
 
+# This function will return a multiline string that introduces the player,
+# giving its full name, the names and UWNetIDs of its creators.
 def introduce():
     return "I'm Nobachess, I don't play baroque chess at this time."
 
 def prepare(player2Nickname):
     pass
 
+# This function will perform a static evaluation of the given state.
+# The value returned should be high if the state is good for WHITE
+# and low if the state is good for BLACK. 
+def staticEval(state):
+    return
 
 test_starting_board()
 
