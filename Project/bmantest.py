e6657c60d2641c6b5aafc2ee37c2d099145c13c9
CHLOE JEAN NASH <chloen2@uw.edu>
2016-05-26 18:52:54 -0700

    Basic movement and killing. You do not die from your own bombs and they have unlimited strength. These are easy changes but kept it this way for testing

--- Project/bmantest.py
+++ Project/bmantest.py
@@ -3,11 +3,15 @@ import random
 
 PLAYER1 = 1
 PLAYER2 = 2
-PLAYER1_BOMB = 5
-PLAYER2_BOMB = 8
+PLAYER1_BOMBS = [5,4,3]
+PLAYER2_BOMBS = [8,7,6]
+EXPLODE = [2,5]
+BARRIER = 20
 EMPTY = 0
+DEAD = 50
 DROP_BOMB = 1
 DONT_DROP_BOMB = 0
+BOARD_SIZE = 11
 
 ACTIONS = ['North', 'South', 'East', 'West', 'Stay']
 
@@ -18,12 +22,12 @@ CODE_TO_INIT = {10:'XX', 20:'##', 1:'AA', 2:'BB', 5:'A3', 4:'A2',
 
 def parse(bs): # bs is board string
   '''Translate a board string into the list of lists representation.'''
-  b = [[0,0,0,0,0,0,0,0,0,0,0] for r in range(11)]
+  b = [[0,0,0,0,0,0,0,0,0,0,0] for r in range(BOARD_SIZE)]
   rs12 = bs.split("\n")
   rs11 = rs12[1:] # eliminate the empty first item.
-  for iy in range(11):
+  for iy in range(BOARD_SIZE):
     rss = rs11[iy].split(' ');
-    for jx in range(11):
+    for jx in range(BOARD_SIZE):
       b[iy][jx] = INIT_TO_CODE[rss[jx]]
   return b
 
@@ -49,8 +53,8 @@ class Bman_state:
 
   def __repr__(self):
     s = ''
-    for r in range(11):
-      for c in range(11):
+    for r in range(BOARD_SIZE):
+      for c in range(BOARD_SIZE):
         s += CODE_TO_INIT[self.board[r][c]] + " "
       s += "\n"
     if self.whose_move==PLAYER1: s+= "Player2's move"
@@ -80,18 +84,91 @@ def transformState(old_state, old_location, move):
   new_location = move[0]
   oldx,oldy = old_location
   newx,newy = new_location
+  new_board = transformDeaths(new_board)
   new_board[newx][newy] = new_board[oldx][oldy]
   if move[1] == DROP_BOMB:
-    player_bomb = PLAYER1_BOMB
+    player_bomb = PLAYER1_BOMBS[0] 
     if old_state.whose_move == PLAYER2:
-      player_bomb = PLAYER2_BOMB
+      player_bomb = PLAYER2_BOMBS[0] 
     new_board[oldx][oldy] = player_bomb
   elif old_location == move[0]:
     new_board[oldx][oldy] = new_board[oldx][oldy]
   else :
     new_board[oldx][oldy] = EMPTY
-  return Bman_state(new_board, old_state.whose_move)
-  
+  new_state = transformBombs(Bman_state(new_board, old_state.whose_move))
+  return new_state
+
+def transformDeaths(board):
+  new_board = [r[:] for r in board]
+  for r in range(BOARD_SIZE):
+    for c in range(BOARD_SIZE):
+      if new_board[r][c] == DEAD:
+        new_board[r][c] = EMPTY
+  return new_board
+
+def transformBombs(state):
+  new_board = [r[:] for r in state.board]
+  players = PLAYER1_BOMBS
+  if state.whose_move == PLAYER2:
+    players = PLAYER2_BOMBS
+  for r in range(BOARD_SIZE):
+    for c in range(BOARD_SIZE):
+      if new_board[r][c] in players:
+        new_bomb = new_board[r][c] - 1
+        if new_bomb in EXPLODE:
+          new_board = transformExplode(state, r, c)
+        else:
+          new_board[r][c] = new_bomb
+  return Bman_state(new_board, state.whose_move)
+
+def transformExplode(state, x, y):
+  new_board = [r[:] for r in state.board]
+  clear = True
+  currx = x
+  curry = y
+  other_player = PLAYER2
+  if state.whose_move == PLAYER2:
+    other_player = PLAYER1
+  new_board[currx][curry] = DEAD
+  while clear:
+    if currx + 1 < BOARD_SIZE - 1:
+      currx += 1
+      if new_board[currx][y] == EMPTY or new_board[currx][y] == other_player or new_board[currx][y] == BARRIER:
+        new_board[currx][y] = DEAD
+      else:
+        break
+    else:
+      break
+  currx = x
+  while clear:
+    if currx - 1 > 0:
+      currx -= 1
+      if new_board[currx][y] == EMPTY or new_board[currx][y] == other_player or new_board[currx][y] == BARRIER:
+        new_board[currx][y] == DEAD
+      else:
+        break
+    else:
+      break
+  while clear:
+    if curry + 1 < BOARD_SIZE - 1:
+      curry += 1
+      if new_board[x][curry] == EMPTY or new_board[x][curry] == other_player or new_board[x][curry] == BARRIER:
+        new_board[x][curry] = DEAD
+      else:
+        break
+    else:
+      break
+  curry = y
+  while clear:
+    if curry - 1 > 0:
+      curry -= 1
+      if new_board[x][curry] == EMPTY or new_board[x][curry] == other_player or new_board[x][curry] == BARRIER:
+        new_board[x][curry] = DEAD
+      else:
+        break
+    else:
+      break
+  return new_board
 
 # assumes player is in a legitimate board position and returns list of
 # actions that the player can take
@@ -119,8 +196,8 @@ def canBomb(actions, player_location):
   return moves
 
 def getPlayerLocation(state):
-  for r in range(11):
-    for c in range(11):
+  for r in range(BOARD_SIZE):
+    for c in range(BOARD_SIZE):
       if state.board[r][c] == state.whose_move:
         return (r,c)
   return (0,0)
@@ -130,26 +207,36 @@ def checkWin(state, player):
   player_check = PLAYER2
   if player == PLAYER2:
     player_check = PLAYER1
-  for r in range(11):
-    for c in range(11):
+  for r in range(BOARD_SIZE):
+    for c in range(BOARD_SIZE):
       if curr_board[r][c] == player_check:
         return False
   print("Player" + str(player) + " won!")
   return True
 
 
-print("Initial board:")
-print(INITIAL)
-print("Board represented as array:")
-print(str(parse(INITIAL)))
-init_state = Bman_state(parse(INITIAL), PLAYER1)
-print("Board converted back to string:")
-print(init_state)
+def play():
+  print("Initial board:")
+  print(INITIAL)
+  print("Board represented as array:")
+  print(str(parse(INITIAL)))
+  init_state = Bman_state(parse(INITIAL), PLAYER1)
+  print("Board converted back to string:")
+  print(init_state)
 
-#print("Check win function:")
-#checkWin(init_state, PLAYER1)
-print("Make move function:")
-makeMove(init_state)
+  #print("Check win function:")
+  #checkWin(init_state, PLAYER1)
+
+  curr = init_state
+  move = 1
+  while not checkWin(curr, PLAYER1):
+    print("Make move function " + str(move) + " :")
+    new = makeMove(curr)
+    curr = new
+    move += 1
+  print("Took " + str(move) + " moves to win")
+
+play()
 
 FINISHED = False
 
