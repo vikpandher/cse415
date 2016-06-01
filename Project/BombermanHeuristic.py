08c7f466f8e954acf0018c181af56b863e09ab61
CHLOE JEAN NASH <chloen2@uw.edu>
2016-05-31 23:23:45 -0700

    comments

--- Project/BombermanHeuristic.py
+++ Project/BombermanHeuristic.py
@@ -4,33 +4,38 @@ import random
 import BombermanSource as bs
 import testBoards as boards
 
-EMPTY = 0
-MAX_BOMB_NUM = 1
-A_BOMBS = [43,42,41]
-B_BOMBS = [53,52,51]
+EMPTY = 0 # if a board space is not occupied
+MAX_BOMB_NUM = 1 # max number of bombs a player can place at once, can be changed
+A_BOMBS = [44,43,42,41] # bombs corresponding to player A
+B_BOMBS = [54,53,52,51] # bombs corresponding to player B
 
+# This method takes the current state and returns the next state that is the
+# result of making the best possible move 
 def makeMove(curr_state):
     move_choice = decide_best(curr_state, curr_state, 1, 6, -1000000, 1000000)
-    #print("orig: " + str(curr_state.board) + " val: " + str(static_eval(curr_state)))
-    #print("move: " + str(move_choice[1].board) + " val: " + str(static_eval(move_choice[1])))
     return move_choice[2]
 
+# This method recursively uses minimax with alpha beta pruning to determine the best
+# possible action choice for the player.  Ply initially set to 6, can be adjusted.
 def decide_best(state, first, level, plyLeft, alpha, beta):
     if plyLeft == 0: return [static_eval(state), state, first, level]
     if state.player == bs.PLAYER_A:
         provisional = [-100000, state, first, level]
-    else:
+    else: # bs.PLAYER_B
         provisional = [100000, state, first, level]
     for s in bs.look_for_successors(state):
         s2 = bs.Bman_state(s.board, s.turn_count, state.player, s.bomb_count)
         if find_location(state) != find_location(s2):
             if level == 1:
+                # stores the first move in the chain of moves that the
+                # player will optimally make
                 first = s
+            # recursively calls decide_best until ply run out
             new = decide_best(s, first, level+1, plyLeft-1, alpha, beta)
             if (state.player == bs.PLAYER_A and new[0] > provisional[0]) or\
                (state.player == bs.PLAYER_B and new[0] < provisional[0]):
                 provisional = new
-                
+            # alpha-beta pruning using fail-soft variation
             if state.player == bs.PLAYER_A:
                 if provisional[0] > alpha:
                     alpha = provisional[0]
@@ -41,7 +46,7 @@ def decide_best(state, first, level, plyLeft, alpha, beta):
                     beta = provisional[0]
                 if beta < alpha:
                     break # alpha cut off
-                   
+            # small amount of randomness among choices w/ same value
             if (state.player == bs.PLAYER_A and new[0] == provisional[0]) or\
                (state.player == bs.PLAYER_B and new[0] == provisional[0]):
                 rint = random.randint(0,1)
@@ -49,14 +54,14 @@ def decide_best(state, first, level, plyLeft, alpha, beta):
                     provisional = new
     return provisional
 
-# good things are not bombs (minus if gunna die),
-# if you are close to an enemy bomb then thats bad,
-# if your bomb is close to an enemy player thats good,
-# less walls (minus points per wall),
-# better in the center (higher value for center pieces),
-# win if opponent is dead,
-# if you are dead then lose
-
+# This function makes a static evaluation of the current state.  If the current player
+# is dead that is maximally worst score and if other player is dead that gives the
+# maximally best score.  A is given a high score as the maximizing player and B is
+# given a low score as a minimizing player.  The score is better if closer to the center
+# of the board as opposed to the outside and planting a bomb is better than not. The
+# euclidean distance is used to try to minimize the distance between the current players
+# bombs and the enemy piece thus incentivizing blowing up the enemy (the end goal). There
+# is a penalty that is proportionate to the danger of being next to an enemy bomb.
 def static_eval(state):
     state_value = 0
     other_state = state
@@ -69,8 +74,6 @@ def static_eval(state):
         other_state = bs.Bman_state(state.board, state.turn_count, other_player, state.bomb_count)
     location = find_location(state)
     other_location = find_location(other_state)
-   # print("location of player " + str(state.player) + " is " + str(location))
-    #print("location of player " + str(other_player) + " is " + str(other_location))
     if location == (0,0): # player is dead
         if state.player == bs.PLAYER_A:
             state_value += -100000
@@ -84,22 +87,16 @@ def static_eval(state):
     else: # player is still alive
         state_value += get_location_value(location, state.player)
         state_value += get_location_value(other_location, other_player)
-     #   print("location val of player " + str(state.player) + " is " + str(get_location_value(location, state.player)))
-      #  print("location val of player " + str(other_player) + " is " + str(get_location_value(other_location, other_player)))
-
-    state_value += get_euc_dist(state, location, other_location)
+        state_value += get_euc_dist(state, location, other_location)
     state_value += get_euc_dist(other_state, other_location, location)
-    
     state_value += check_bomb_proximity(state, state.player, location)
     state_value += check_bomb_proximity(other_state, other_player, other_location)
-   # print("bomb proximity val of player " + str(state.player) + " is " + str(check_bomb_proximity(state, state.player, location)))
-    #print("bomb proximity val of player " + str(other_player) + " is " + str(check_bomb_proximity(other_state, other_player, other_location)))
     state_value += bomb_nums(state)
     state_value += bomb_nums(other_state)
-    #print("bomb nums val of player " + str(state.player) + " is " + str(bomb_nums(state)))
-    #print("bomb nums val of player " + str(other_player) + " is " + str(bomb_nums(other_state)))
     return state_value
 
+# Determines the sum of the euclidean distance between all of current player's bombs and
+# the enemy player.
 def get_euc_dist(state, friend, enemy):
     value = 0
     row, col = enemy
@@ -115,7 +112,6 @@ def get_euc_dist(state, friend, enemy):
                 value -= max_dist - curr_dist
     return value
                 
-
 # good to plant bombs
 def bomb_nums(state):
     value = (MAX_BOMB_NUM - state.bomb_count[state.player]) * 10
@@ -179,20 +175,8 @@ def check_bomb_proximity(state, player, location):
         else:
             break
     if state.player == bs.PLAYER_A:
-        '''
-        if state.player == player:
-            other_state = bs.Bman_state(state.board, state.turn_count, bs.PLAYER_B, state.bomb_count)
-            if check_bomb_proximity(state, bs.PLAYER_B, find_location(other_state)) >= 0:
-                value += 1
-                '''
         return value
     else: # bs.PLAYER_B
-        if state.player == player:
-            '''
-            other_state = bs.Bman_state(state.board, state.turn_count, bs.PLAYER_A, state.bomb_count)
-            if check_bomb_proximity(state, bs.PLAYER_A, find_location(other_state)) <= 0:
-                value -= 1
-                '''
         return 0 - value
 
 # returns a greater absolute value if closer to the center of the board
@@ -206,7 +190,8 @@ def get_location_value(location, player):
         return value
     else: # bs.PLAYER_B
         return 0 - value
-    
+
+# returns the row,col of the current player as a tuple
 def find_location(state):
     for row in range(bs.BOARD_SIZE):
         for col in range(bs.BOARD_SIZE):
@@ -214,27 +199,6 @@ def find_location(state):
             if state.board[row][col] == (state.player* 10 + bs.PLAYER_CODE_OFFSET):
                 return (row, col)
     return (0, 0)
-'''
-#print(bs.Bman_state(bs.create_initial_board()))
-print(static_eval(bs.Bman_state(bs.create_initial_board())))
-initial = bs.Bman_state(bs.create_initial_board())
-for s in bs.look_for_successors(initial):
-    s2 = bs.Bman_state(s.board, s.turn_count, initial.player, s.bomb_count)
-    if find_location(initial) != find_location(s2):
-        print(s)
-        print(static_eval(s))
-
-
-
-'''
-
-
-
-
-
-
-
-
 
 
 
